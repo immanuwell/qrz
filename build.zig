4e62fb516a14402ebcb4e1d51e2fe3ce5bf7a680
immanuwell <pchpr.00@list.ru>
2025-12-18 09:41:35 +0400

    chore: small changes in the additional files like build.zig

--- build.zig
+++ build.zig
@@ -70,16 +70,6 @@ pub fn build(b: *std.Build) void {
             // definition if desireable (e.g. firmware for embedded devices).
             .target = target,
             .optimize = optimize,
-            // List of modules available for import in source files part of the
-            // root module.
-            .imports = &.{
-                // Here "qrz" is the name you will use in your source code to
-                // import this module (e.g. `@import("qrz")`). The name is
-                // repeated because you are allowed to rename your imports, which
-                // can be extremely useful in case of collisions (which can happen
-                // importing modules from different packages).
-                .{ .name = "qrz", .module = mod },
-            },
         }),
     });
 
